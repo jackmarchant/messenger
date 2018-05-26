34cc0036da9c19a6ed1caac7219b9df87366fa98
Jack Marchant <jack@expert360.com>
2018-05-26 23:41:57 +1000

    real time messaging

--- frontend/src/Thread.js
+++ frontend/src/Thread.js
@@ -1,41 +1,152 @@
 import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
-import { graphql, QueryRenderer } from 'react-relay';
+import { graphql, QueryRenderer, createRefetchContainer } from 'react-relay';
 import modernEnvironment from './environment';
 import RelayRenderContainer from './RelayRenderContainer';
 
-import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import CreateMessageMutation from './CreateMessageMutation';
+
 import './Message.css';
 
 const Message = ({ content }) => {
   return (
     <Fragment>
       <p className="message">{content}</p>
-      <Divider />
     </Fragment>
   );
 };
 
-const Thread = ({ name, messages }) => {
-  return (
-    <Fragment>
-      <NavLink to="/">Back to threads</NavLink>
-      <h2>Your conversation</h2>
-      <div className="messages">
-        {messages.map((message, key) => <Fragment key={`message-${key}`}><Message {...message} /></Fragment>)}
-      </div>
-    </Fragment>
-  );
+class MessageBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.messageField = null;
+    this.state = {
+      message: '',
+    };
+  }
+
+  sendMessage(userId, threadId) {
+    const content = this.state.message;
+    this.setState({ message: '' }, () => {
+      CreateMessageMutation.commit({
+        userId,
+        threadId,
+        content,
+      }).then(response => {
+        this.props.onSendMessage(response);
+        this.messageField.input.scrollIntoView();
+      });
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    // replace with current user user id
+    this.sendMessage('VXNlcjoy', this.props.threadId);
+    return false;
+  }
+
+  render() {
+    return (
+      <form autoComplete="off" onSubmit={this.onSubmit}>
+        <TextField
+          ref={element => this.messageField = element}
+          id="your-message"
+          name="your-message"
+          floatingLabelText="Your message"
+          fullWidth
+          onChange={(e, message) => this.setState({ message })}
+          value={this.state.message}
+        />
+      </form>
+    );
+  }
 };
 
-const query = graphql`
-  query ThreadQuery($slug: String!) {
-    thread(slug: $slug) {
-      id
-      messages {
-        content
+class Thread extends React.Component {
+  constructor(props) {
+    super(props);
+    this.refetchData = this.refetchData.bind(this);
+    this.createMessagesListRef = this.createMessagesListRef.bind(this);
+    this.scrollMessagesList = this.scrollMessagesList.bind(this);
+    this.messagesList = null;
+  }
+
+  createMessagesListRef(messagesList) {
+    if (messagesList) {
+      messagesList.scrollIntoView(true);
+      messagesList.scrollTop = messagesList.scrollHeight;
+      this.messagesList = messagesList;
+    }
+  }
+
+  scrollMessagesList() {
+    this.messagesList.scrollTop = this.messagesList.scrollHeight;
+  }
+
+  refetchData(slug) {
+    this.props.relay.refetch(
+      { slug },
+      null,
+      this.scrollMessagesList,
+      { force: true }
+    );
+  }
+
+  render() {
+    const { name, messages, id, slug } = this.props.data.thread;
+
+    return (
+      <Fragment>
+        <NavLink to="/">Back to threads</NavLink>
+        <h2>Your conversation</h2>
+        <div className="messages" ref={this.createMessagesListRef}>
+          {messages.map((message, key) => <Fragment key={`message-${key}`}><Message {...message} /></Fragment>)}
+        </div>
+        <MessageBox threadId={id} onSendMessage={() => this.refetchData(slug)} />
+      </Fragment>
+    );
+  }
+}
+
+
+const RefetchableThread = createRefetchContainer(
+  Thread,
+  graphql`
+    fragment Thread on Query
+    @argumentDefinitions(
+      slug: { type: String }
+    ) {
+      thread(slug: $slug) {
+        id
+        slug
+        messages {
+          content
+        }
       }
     }
+  `,
+  graphql`
+    query ThreadRefetchQuery(
+      $slug: String!
+    ) {
+      ...Thread
+        @arguments(slug: $slug)
+    }
+  `
+);
+
+const query = graphql`
+  query ThreadQuery(
+    $slug: String!
+  ) {
+    ...Thread
+      @arguments(slug: $slug)
   }
 `;
 
@@ -46,7 +157,7 @@ const ThreadRenderer = ({ match }) => {
       query={query}
       variables={{slug: match.params.slug}}
       render={RelayRenderContainer(props => {
-        return <Thread {...props.data.thread} />;
+        return <RefetchableThread {...props} />;
       })}
     />
   );
